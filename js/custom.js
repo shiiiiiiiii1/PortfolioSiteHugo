a9540dad610844bb8d7a573c0d86c7cd3cd1530f
shiiiiiiiii1 <shou1_822@yahoo.co.jp>
2017-03-06 17:36:41 +0900

    trial pages public

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+var url_mine = window.location.href.split("/")[3];   // アドレスを取得し、"/"で区切り配列にし、その4番目の値を取得する。
+if (url_mine == "") {
+  document.getElementById("menu-home-mine").classList.add('active');
+} else if(url_mine == "about") {
+  document.getElementById("menu-about-mine").classList.add('active');
+} else if(url_mine == "contact") {
+  document.getElementById("menu-contact-mine").classList.add('active');
+}
