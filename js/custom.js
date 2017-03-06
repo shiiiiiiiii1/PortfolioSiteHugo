a759d386940624a9c0ba45ef43b2cabf0de88f1f
shiiiiiiiii1 <shou1_822@yahoo.co.jp>
2017-03-06 19:11:10 +0900

    bg color

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,9 @@
 var url_mine = window.location.href.split("/")[4];   // アドレスを取得し、"/"で区切り配列にし、その5番目の値を取得する。
-if (url_mine == "") {
+if (url_mine == "") {   // homeページ
   document.getElementById("menu-home-mine").classList.add('active');
-} else if(url_mine == "about") {
+  document.getElementById("copyright-p-mine").style.backgroundColor = "#f4f4f4";
+} else if(url_mine == "about") {   // aboutページ
   document.getElementById("menu-about-mine").classList.add('active');
-} else if(url_mine == "contact") {
+} else if(url_mine == "contact") {   // contactページ
   document.getElementById("menu-contact-mine").classList.add('active');
 }
