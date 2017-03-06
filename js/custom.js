5a53f2bb4aac6868c3eb0113356d6c897b0f05e1
shiiiiiiiii1 <shou1_822@yahoo.co.jp>
2017-03-06 18:37:59 +0900

    trial pages public

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-var url_mine = window.location.href.split("/")[3];   // アドレスを取得し、"/"で区切り配列にし、その4番目の値を取得する。
+var url_mine = window.location.href.split("/")[4];   // アドレスを取得し、"/"で区切り配列にし、その5番目の値を取得する。
 if (url_mine == "") {
   document.getElementById("menu-home-mine").classList.add('active');
 } else if(url_mine == "about") {
